598a9bb43d1b17ab0a5e58ab8686eb4f25c6c210
LingFenglong <2808021998@qq.com>
2023-08-07 22:17:33 +0800

    all

--- MyBatisPlusDynamicDataSource/src/test/java/com/lingfenglong/mybatisplusdynamicdatasource/MyBatisPlusDynamicDataSourceApplicationTests.java
+++ MyBatisPlusDynamicDataSource/src/test/java/com/lingfenglong/mybatisplusdynamicdatasource/MyBatisPlusDynamicDataSourceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.lingfenglong.mybatisplusdynamicdatasource;
 
+import com.lingfenglong.mybatisplusdynamicdatasource.mapper.ProductMapper;
+import com.lingfenglong.mybatisplusdynamicdatasource.mapper.UserMapper;
 import com.lingfenglong.mybatisplusdynamicdatasource.service.ProductService;
 import com.lingfenglong.mybatisplusdynamicdatasource.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -9,8 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyBatisPlusDynamicDataSourceApplicationTests {
 
+//    private UserMapper userMapper;
+//    private ProductMapper productMapper;
     private UserService userService;
     private ProductService productService;
+
+//    @Autowired
+//    public MyBatisPlusDynamicDataSourceApplicationTests(UserMapper userMapper, ProductMapper productMapper) {
+//        this.userMapper = userMapper;
+//        this.productMapper = productMapper;
+//    }
 
     @Autowired
     public MyBatisPlusDynamicDataSourceApplicationTests(UserService userService, ProductService productService) {
@@ -22,6 +32,9 @@ class MyBatisPlusDynamicDataSourceApplicationTests {
     void contextLoads() {
         System.out.println(userService.getById(1));
         System.out.println(productService.getById(1));
+
+//        System.out.println(userMapper.selectById(1));
+//        System.out.println(productMapper.selectById(1));
     }
 
 }
